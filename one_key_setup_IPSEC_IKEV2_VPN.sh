dddaa15e0e6402a254c3b0abad30cab6ecc9ec35
nealy <nealy520@gmail.com>
2016-01-22 15:17:46 +0800

    Create one_key_setup_IPSEC_IKEV2_VPN.sh

---
+++ one_key_setup_IPSEC_IKEV2_VPN.sh
@@ -0,0 +1,389 @@
+#!/bin/bash
+
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+###############################################################
+#   System Required: CentOS6.x (32bit/64bit) or Ubuntu
+#   Description: Install IPSEC/IKEV2 VPN for CentOS/Ubuntu
+#   Author: nealy (http://www.mynook.xyz)
+###############################################################
+
+clear
+echo "#############################################################"
+echo "# Install IPSEC/IKEV2 VPN for CentOS6.x (32bit/64bit) or Ubuntu"
+echo "#"
+echo "# Author: nealy"
+echo "# Site: http://www.mynook.xyz"
+echo "#"
+echo "#############################################################"
+echo ""
+
+function setup() {
+	permission_check
+	disable_selinux
+	get_server_ip
+	get_server_os
+	get_pretreatment
+	install_depend
+	download_strongswan
+	setup_strongswan
+	setup_cert
+	configure_ipsec
+	configure_strongswan
+	configure_secrets
+	configure_iptables
+	ipsec start
+	show_success_info
+}
+
+# Must run as root
+function permission_check() {
+	if [[ $EUID -ne 0 ]]; then
+	   echo "Error: This script must be run as root!" 1>&2
+	   exit 1
+	fi
+}
+
+function disable_selinux() {
+	if [ -s /etc/selinux/config ] && grep 'SELINUX=enforcing' /etc/selinux/config; then
+	    sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+	    setenforce 0
+	fi
+}
+
+# Get IP address of the server
+function get_server_ip() {
+    echo "Preparing, Please wait for a moment..."
+    IP=`curl -s checkip.dyndns.com | cut -d' ' -f 6  | cut -d'<' -f 1`
+    if [ -z $IP ]; then
+        IP=`curl -s ifconfig.me/ip`
+    fi
+}
+
+function get_server_os() {
+	get_system_str=`cat /etc/issue`
+	echo "$get_system_str" |grep -q "CentOS"
+	if  [ $? -eq 0 ]
+	then
+		system_str="0"
+	else
+		echo "$get_system_str" |grep -q "Ubuntu"
+		if [ $? -eq 0 ]
+		then
+			system_str="1"
+		else
+			echo "The system must be CentOS or Ubuntu"
+			exit 1
+		fi
+	fi
+}
+
+function get_pretreatment() {
+    echo "Please choose the type of your VPS(Xen、KVM: 1,  OpenVZ: 2):"
+    read -p "type of VPS(1 or 2):" input_type
+    if [ "$input_type" = "1" ]; then
+        vps_type="1"
+		vps_str="Xen、KVM"
+		else
+			if [ "$input_type" = "2" ]; then
+				vps_type="2"
+				vps_str="OpenVZ"
+				else
+					echo "Unknown VPS type: $input_type"
+				exit 1
+			fi
+    fi
+		echo "Please input the ip (or domain) of your VPS:"
+	    read -p "ip or domain(default_vale:${IP}):" vps_ip
+	if [ "$vps_ip" = "" ]; then
+		vps_ip=$IP
+	fi
+		echo "Please input the cert country(C):"
+	    read -p "C(default value:com):" my_cert_c
+	if [ "$my_cert_c" = "" ]; then
+		my_cert_c="com"
+	fi
+		echo "Please input the cert organization(O):"
+	    read -p "O(default value:vpn):" my_cert_o
+	if [ "$my_cert_o" = "" ]; then
+		my_cert_o="vpn"
+	fi
+		echo "Please input the cert common name(CN):"
+		echo "(For iOS/OS X user, server ip recommended)"
+	    read -p "CN(default value:VPN CA):" my_cert_cn
+	if [ "$my_cert_cn" = "" ]; then
+		my_cert_cn="VPN CA"
+	fi
+	echo "Done! Thanks!"
+
+    get_char() {
+        SAVEDSTTY=`stty -g`
+        stty -echo
+        stty cbreak
+        dd if=/dev/tty bs=1 count=1 2> /dev/null
+        stty -raw
+        stty echo
+        stty $SAVEDSTTY
+    }
+    echo "Please confirm your settings:"
+	echo ""
+	echo -e "Type of your server: [\033[32;1m$vps_str\033[0m]"
+	echo -e "IP(or domain) of your server: [\033[32;1m$vps_ip\033[0m]"
+	echo -e "Cert info:[\033[32;1mC=${my_cert_c}, O=${my_cert_o}\033[0m]"
+	echo ""
+    echo "Press any key to start...or Press Ctrl+C to cancel"
+	char=`get_char`
+	#Current folder
+    cur_dir=`pwd`
+    cd $cur_dir
+}
+
+function install_depend() {
+	if [ "$system_str" = "0" ]; then
+		yum -y update
+		yum -y install pam-devel openssl-devel make gcc
+	else
+		apt-get -y update
+		apt-get -y install libpam0g-dev libssl-dev make gcc
+	fi
+}
+
+function download_strongswan() {
+    if [ -f strongswan.tar.gz ];then
+        echo -e "strongswan.tar.gz [\033[32;1mfound\033[0m]"
+    else
+        if ! wget https://download.strongswan.org/strongswan-5.3.5.tar.gz;then
+            echo "Failed to download strongswan.tar.gz"
+            exit 1
+        fi
+    fi
+    tar xzf strongswan*.tar.gz
+    if [ $? -eq 0 ];then
+        cd $cur_dir/strongswan-*/
+    else
+        echo ""
+        echo "Unzip strongswan.tar.gz failed! Please visit http://quericy.me/blog/699 and contact."
+        exit 1
+    fi
+}
+
+# Make and Install strongswan
+function setup_strongswan() {
+	if [ "$vps_type" = "1" ]; then
+		./configure  --enable-openssl --enable-nat-transport \
+--disable-mysql --disable-ldap --disable-static --enable-shared \
+--enable-md4 --enable-eap-mschapv2 --enable-eap-aka --enable-eap-aka-3gpp2 \
+--enable-eap-gtc --enable-eap-identity --enable-eap-md5 --enable-eap-peap \
+--enable-eap-radius --enable-eap-sim --enable-eap-sim-file --enable-eap-simaka-pseudonym \
+--enable-eap-simaka-reauth --enable-eap-simaka-sql --enable-eap-tls --enable-eap-tnc --enable-eap-ttls
+	else
+		./configure  --enable-eap-identity --enable-eap-md5 \
+--enable-eap-mschapv2 --enable-eap-tls --enable-eap-ttls --enable-eap-peap \
+--enable-eap-tnc --enable-eap-dynamic --enable-eap-radius --enable-xauth-eap \
+--enable-xauth-pam --enable-dhcp --enable-openssl --enable-addrblock --enable-unity \
+--enable-certexpire --enable-radattr --enable-tools --enable-openssl --disable-gmp --enable-kernel-libipsec
+	fi
+	make; make install
+}
+
+function setup_cert() {
+	cd $cur_dir
+
+    if [ -f ca.pem ];then
+        echo -e "ca.pem [\033[32;1mfound\033[0m]"
+    else
+        echo -e "ca.pem [\033[32;1mauto create\032[0m]"
+		echo "auto create ca.pem ..."
+		ipsec pki --gen --outform pem > ca.pem
+    fi
+	if [ -f ca.cert.pem ];then
+        echo -e "ca.cert.pem [\033[32;1mfound\033[0m]"
+    else
+        echo -e "ca.cert.pem [\032[33;1mauto create\032[0m]"
+		echo "auto create ca.cert.pem ..."
+		ipsec pki --self --in ca.pem --dn "C=${my_cert_c}, O=${my_cert_o}, CN=${my_cert_cn}" --ca --outform pem >ca.cert.pem
+    fi
+
+	if [ ! -d my_key ];then
+        mkdir my_key
+    fi
+	mv ca.pem my_key/ca.pem
+	mv ca.cert.pem my_key/ca.cert.pem
+	cd my_key
+	ipsec pki --gen --outform pem > server.pem
+	ipsec pki --pub --in server.pem | ipsec pki --issue --cacert ca.cert.pem \
+--cakey ca.pem --dn "C=${my_cert_c}, O=${my_cert_o}, CN=${vps_ip}" --san="${vps_ip}" \
+--flag serverAuth --flag ikeIntermediate --outform pem > server.cert.pem
+	ipsec pki --gen --outform pem > client.pem
+	ipsec pki --pub --in client.pem | ipsec pki --issue --cacert ca.cert.pem --cakey ca.pem \
+--dn "C=${my_cert_c}, O=${my_cert_o}, CN=VPN Client" --outform pem > client.cert.pem
+	echo "Please input the PKCS12 cert password(Can be empty):"
+	openssl pkcs12 -export -inkey client.pem -in client.cert.pem -name "client" -certfile ca.cert.pem -caname "${my_cert_cn}"  -out client.cert.p12
+
+    get_char() {
+        SAVEDSTTY=`stty -g`
+        stty -echo
+        stty cbreak
+        dd if=/dev/tty bs=1 count=1 2> /dev/null
+        stty -raw
+        stty echo
+        stty $SAVEDSTTY
+    }
+    echo "Install the VPN cert"
+	cp -r ca.cert.pem /usr/local/etc/ipsec.d/cacerts/
+	cp -r server.cert.pem /usr/local/etc/ipsec.d/certs/
+	cp -r server.pem /usr/local/etc/ipsec.d/private/
+	cp -r client.cert.pem /usr/local/etc/ipsec.d/certs/
+	cp -r client.pem  /usr/local/etc/ipsec.d/private/
+}
+
+function configure_ipsec() {
+	cat > /usr/local/etc/ipsec.conf<<-EOF
+config setup
+    uniqueids=never
+
+conn %default
+    keyexchange=ike
+    left=%any
+    leftsubnet=0.0.0.0/0
+    right=%any
+
+conn IKE-BASE
+    leftcert=server.cert.pem
+    rightsourceip=10.0.0.0/24
+
+# ios etc.
+conn by_cert
+    also=IKE-BASE
+    keyexchange=ikev1
+    fragmentation=yes
+    leftauth=pubkey
+    leftsubnet=0.0.0.0/0
+    rightauth=pubkey
+    rightauth2=xauth
+    rightcert=client.cert.pem
+    auto=add
+# ios etc.
+conn by_psk
+    also=IKE-BASE
+    keyexchange=ikev1
+    leftauth=psk
+    rightauth=psk
+    rightauth2=xauth
+    auto=add
+
+# osx linux android etc.
+conn by_key
+    also=IKE-BASE
+    keyexchange=ikev2
+    leftauth=pubkey
+    rightauth=pubkey
+    rightcert=client.cert.pem
+    auto=add
+
+# ikev2 (ios osx win7 etc.)
+conn IKEv2-EAP
+    also=IKE-BASE
+    keyexchange=ikev2
+    ike=aes256-sha256-modp1024,3des-sha1-modp1024,aes256-sha1-modp1024!
+    esp=aes256-sha256,3des-sha1,aes256-sha1!
+    rekey=no
+    leftid=${my_cert_cn}
+    leftauth=pubkey
+    leftsendcert=always
+    rightfirewall=yes
+    rightsendcert=never
+    rightauth=eap-mschapv2
+    eap_identity=%any
+    dpdaction=clear
+    fragmentation=yes
+    auto=add
+
+EOF
+}
+
+function configure_strongswan() {
+ cat > /usr/local/etc/strongswan.conf<<-EOF
+charon {
+    load_modular = yes
+    duplicheck.enable = no
+    compress = yes
+    plugins {
+        include strongswan.d/charon/*.conf
+    }
+    dns1 = 8.8.8.8
+    dns2 = 8.8.4.4
+    nbns1 = 8.8.8.8
+    nbns2 = 8.8.4.4
+}
+include strongswan.d/*.conf
+EOF
+}
+
+function configure_secrets() {
+	cat > /usr/local/etc/ipsec.secrets<<-EOF
+: RSA server.pem
+: PSK "myPSKpw"
+: XAUTH "myXAUTHpw"
+myUserName %any : EAP "myUserPassWord"
+EOF
+}
+
+function configure_iptables() {
+	sysctl -w net.ipv4.ip_forward=1
+	if [ "$vps_type" = "1" ]; then
+		iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
+		iptables -A FORWARD -s 10.0.0.0/24  -j ACCEPT
+		iptables -A INPUT -i eth0 -p esp -j ACCEPT
+		iptables -A INPUT -i eth0 -p udp --dport 500 -j ACCEPT
+		iptables -A INPUT -i eth0 -p tcp --dport 500 -j ACCEPT
+		iptables -A INPUT -i eth0 -p udp --dport 4500 -j ACCEPT
+		iptables -A INPUT -i eth0 -p udp --dport 1701 -j ACCEPT
+		iptables -A INPUT -i eth0 -p tcp --dport 1723 -j ACCEPT
+		iptables -A FORWARD -j REJECT
+		iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -o eth0 -j MASQUERADE
+	else
+		iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
+		iptables -A FORWARD -s 10.0.0.0/24  -j ACCEPT
+		iptables -A INPUT -i venet0 -p esp -j ACCEPT
+		iptables -A INPUT -i venet0 -p udp --dport 500 -j ACCEPT
+		iptables -A INPUT -i venet0 -p tcp --dport 500 -j ACCEPT
+		iptables -A INPUT -i venet0 -p udp --dport 4500 -j ACCEPT
+		iptables -A INPUT -i venet0 -p udp --dport 1701 -j ACCEPT
+		iptables -A INPUT -i venet0 -p tcp --dport 1723 -j ACCEPT
+		iptables -A FORWARD -j REJECT
+		iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -o venet0 -j MASQUERADE
+    fi
+	if [ "$system_str" = "0" ]; then
+		service iptables save
+	else
+		iptables-save > /etc/iptables.rules
+		cat > /etc/network/if-up.d/iptables<<EOF
+#!/bin/sh
+iptables-restore < /etc/iptables.rules
+EOF
+		chmod +x /etc/network/if-up.d/iptables
+	fi
+}
+
+function show_success_info(){
+	echo "#############################################################"
+	echo -e "#"
+	echo -e "# [\033[32;1mInstall Successful\033[0m]"
+	echo -e "# The default login info of your VPN"
+	echo -e "# PSK:\033[33;1m myPSKpw\033[0m"
+	echo -e "# XAUTH:\033[33;1m myXAUTHpw\033[0m"
+	echo -e "# UserName:\033[33;1m myUserName\033[0m"
+	echo -e "# PassWord:\033[33;1m myUserPassWord\033[0m"
+	echo -e "# You can change UserName and PassWord in\033[32;1m /usr/local/etc/ipsec.secrets\033[0m"
+	echo -e "# You must copy the cert \033[32;1m ${cur_dir}/my_key/ca.cert.pem \033[0m to the client and install it"
+	echo -e "# After reboot, you need to start ipsec manually: ipsec start"
+	echo -e "# In some case, you need to set net.ipv4.ip_forward=1 in file /etc/sysctl.conf manually"
+	echo -e "# Run sysctl -p to apply this change."
+	echo -e "#"
+	echo -e "###########################################################"
+	echo -e ""
+}
+
+setup
